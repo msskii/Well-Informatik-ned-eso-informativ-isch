561892ff88ae50b77bd9cdae62ca50a2ecdd4b4e
Aiko-Aaron <aaronhodel@gmail.com>
2018-08-20 10:50:44 +0200

    Window class & SDL include added

--- Informatik/Informatik/graphics/Window.cpp
+++ Informatik/Informatik/graphics/Window.cpp
@@ -1,15 +1,9 @@
 //
-//  main.cpp
+//  Window.cpp
 //  Informatik
 //
 //  Created by Aaron Hodel on 20.08.18.
 //  Copyright © 2018 Aaron Hodel. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, char **args)
-{
-    
-    return 0;
-}
+#include "Window.hpp"
---
+++ Informatik/Informatik/graphics/Window.hpp
@@ -0,0 +1,18 @@
+//
+//  Window.hpp
+//  Informatik
+//
+//  Created by Aaron Hodel on 20.08.18.
+//  Copyright © 2018 Aaron Hodel. All rights reserved.
+//
+
+#ifndef Window_hpp
+#define Window_hpp
+
+#ifdef __APPLE__
+#  include <SDL2/SDL.h> // Other path than on windows
+#else
+#  include <SDL2.h>
+#endif
+
+#endif /* Window_hpp */
