9059a6c8e7a112c1506970881226e07c9f144eeb
Aiko-Aaron <aaronhodel@gmail.com>
2018-08-20 10:44:38 +0200

    Added Xcode project

---
+++ Informatik/Informatik/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Informatik
+//
+//  Created by Aaron Hodel on 20.08.18.
+//  Copyright © 2018 Aaron Hodel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
