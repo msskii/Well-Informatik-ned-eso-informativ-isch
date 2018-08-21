5e51d081e06d24c961d1fb3e4d2f9681cd8e996b
Sirkii <bigsplitt@gmail.com>
2018-08-21 17:02:37 +0200

    included "fuck linux"

--- Informatik/Informatik/util/FileReader.cpp
+++ Informatik/Informatik/util/FileReader.cpp
@@ -11,43 +11,43 @@
 uint8_t* readFile(const char *filePath)
 {
     FILE *f;
-    
+
 #ifdef __APPLE__
     f = fopen(filePath, "rb"); // Apple has the insecure version here
 #else
     fopen_s(f, filePath, "rb"); // Windows has secure
 #endif
-    
+
     if(f == nullptr)
     {
         ERROR("File not found");
         return nullptr; // File not found
     }
-    
+
     fseek(f, 0, SEEK_END);
     size_t fileSize = ftell(f);
     fseek(f, 0, SEEK_SET);
     uint8_t *data = (uint8_t *) malloc(fileSize);
     fread(data, 1, fileSize, f); // Read data
-    
+
     return data;
 }
 
 void writeFile(const char *filePath, uint8_t *dataToWrite, int size)
 {
     FILE *f;
-    
+
 #ifdef __APPLE__
-    f = fopen(filePath, "wb"); // Apple has the insecure version here
+    f = fopen(filePath, "wb"); // Apple has the insecure version here, fuck linux
 #else
-    fopen_s(f, filePath, "wb"); // Windows has secure
+    fopen_s(f, filePath, "wb"); // Windows has securea
 #endif
-    
+
     if(f == nullptr)
     {
         ERROR("File not found");
         return; // File not found
     }
-    
+
     fwrite(dataToWrite, 1, size, f); // Write data
 }
