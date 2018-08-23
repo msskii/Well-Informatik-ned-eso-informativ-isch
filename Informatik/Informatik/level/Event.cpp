483095f1432d4f6ee8d18ddf6b30a196f1890c50
Aiko-Aaron <aaronhodel@gmail.com>
2018-08-23 10:23:39 +0200

    Changed Event counting (start from one) so the decency can be disabled by setting it to 0

--- Informatik/Informatik/level/Event.cpp
+++ Informatik/Informatik/level/Event.cpp
@@ -11,14 +11,15 @@
 #include "loader/EventActions.hpp"
 #include "../entity/Player.hpp"
 
-int event_id_counter = 0;
+int event_id_counter = 1; // Start with id Nr. 1
 std::map<int, Event*> eventList;
 
-Event::Event(SerializedEvent eventData, uint8_t *args) : event_id(event_id_counter++), arguments(args)
+Event::Event(SerializedEvent eventData, uint8_t *args) : arguments(args)
 {
     arguments = args;
     toStore = eventData;
     onTrigger = resolveFunction(toStore.event_action);
+    event_id = eventData.event_id;
     
     eventList[event_id] = this;
 }
@@ -34,12 +35,14 @@ void Event::trigger(Event event, EVENT_TYPE type, Level *level, uint8_t *argumen
     // TODO: Check dependency & amount
     state.timesTriggered++;
     if(state.timesTriggered > toStore.triggerAmount && toStore.triggerAmount != 0) return; // Don't execute, already too much
+    if(toStore.event_id_dependency && eventList[toStore.event_id_dependency] != nullptr && eventList[toStore.event_id_dependency]->toStore.triggerAmount == 0) return;
     onTrigger(event, type, level, arguments);
 }
 
 void Event::render(SDL_Renderer *renderer, int xoffset, int yoffset)
 {
     if(state.timesTriggered > toStore.triggerAmount && toStore.triggerAmount != 0) return;
+    if(toStore.event_id_dependency && eventList[toStore.event_id_dependency] != nullptr && eventList[toStore.event_id_dependency]->toStore.triggerAmount == 0) return;
     
     SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
     SDL_Rect r = {toStore.event_x + xoffset + PLAYER_OFFSET_X, toStore.event_y + yoffset + PLAYER_OFFSET_Y, toStore.event_w, toStore.event_h};
