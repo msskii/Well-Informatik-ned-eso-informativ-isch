264f8641cb65a3ecb7bf8a4fa9c1db28a7cfd4a5
Aiko-Aaron <aaronhodel@gmail.com>
2018-08-30 12:48:28 +0200

    Small optimization (Only render visible tiles)

--- Informatik/Informatik/level/Tile.cpp
+++ Informatik/Informatik/level/Tile.cpp
@@ -46,6 +46,8 @@ void Tile::render(SDL_Renderer *renderer, int xoffset, int yoffset)
     
     SDL_Rect src = {0, 0, 32, 32}; // For individual 32 by 32 tiles
     SDL_Rect dst = {xcoord * TILE_SIZE + xoffset, ycoord * TILE_SIZE + yoffset, TILE_SIZE, TILE_SIZE};
+    
+    if(dst.x >= GAME_WIDTH || dst.x < -TILE_SIZE || dst.y >= GAME_HEIGHT || dst.y < -TILE_SIZE) return; // Only render the visible ones...
     SDL_RenderCopy(renderer, texture, &src, &dst);
     
     /**
