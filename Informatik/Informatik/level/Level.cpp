6da4277ba9991358221b33efd72cbfe46da1d608
Sirkii <bigsplitt@gmail.com>
2018-08-30 09:26:28 +0200

    ....

--- Informatik/Informatik/level/Level.cpp
+++ Informatik/Informatik/level/Level.cpp
@@ -13,7 +13,7 @@ Level::Level(int w, int h) : width(w), height(h), tiles(new Tile[w * h]), player
 {    
     for(int i = 0; i < w * h; i++)
     {
-        if(i%50==0)
+        if((i-5)%50==0)
         {
             tiles[i] = Tile(i % w, i / w, 1);
         }else{
